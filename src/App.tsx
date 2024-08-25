96c0ebe80397bbea7cece601f40b62a38e4e28f8
moritzgross <moritz.gross@digitalfrontiers.de>
2024-08-25 17:23:00 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import RowComponent from "./components/RowComponent";
-import calculateBPEStep from "./utils"
+import calculateBPEStep from "./utils";
 
 const BPEVisualization: React.FC = () => {
-    const originalText = [1, 4, 7, 5, 1, 4, 7];
+  const originalText = [1, 4, 7, 5, 1, 4, 7];
 
-    const others = [originalText];
-    while (others[others.length - 1].length > 1) { // last element should have only 1 token
-        others.push(calculateBPEStep(others[others.length - 1]));
-    }
+  const others = [originalText];
+  while (others[others.length - 1].length > 1) {
+    // last element should have only 1 token
+    others.push(calculateBPEStep(others[others.length - 1]));
+  }
 
-    return (
-        <div>
-            <h2>BPE Visualization:</h2>
-            {others.map((list, index) => (
-                <div key={index}>
-                    <h3>{index === 0 ? "Original Text:" : `Step ${index}:`}</h3>
-                    <RowComponent characters={list} />
-                </div>
-            ))}
+  return (
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      <h2>BPE Visualization:</h2>
+      {others.map((list, index) => (
+        <div
+          key={index}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <div
+            style={{
+              width: "80px",
+              marginRight: "20px",
+              textAlign: "right",
+              fontWeight: "bold",
+            }}
+          >
+            {index === 0 ? "Original:" : `Step ${index}:`}
+          </div>
+          <RowComponent characters={list} />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default BPEVisualization;
